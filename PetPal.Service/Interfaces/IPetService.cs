6fd8273cf0dc5ef8fe86a8e2eabcc9b530ab3e57
Absalyamov Amir <absalyamov.amir@gmail.com>
2023-08-29 16:14:04 +0400

    Writing data to the database

---
+++ PetPal.Service/Interfaces/IPetService.cs
@@ -0,0 +1,11 @@
+﻿using PetPal.Domain.Entity;
+using PetPal.Domain.Response;
+using PetPal.Domain.ViewModels.Pet;
+
+namespace PetPal.Service.Interfaces
+{
+    public interface IPetService
+    {
+        Task<IBaseResponse<PetEntity>> Create(CreatePetViewModel model);
+    }
+}
--- PetPal/Controllers/PetController.cs
+++ PetPal/Controllers/PetController.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PetPal.Domain.ViewModels.Pet;
-
+using PetPal.Service.Interfaces;
 using System.Diagnostics;
 
 namespace PetPal.Controllers
 {
     public class PetController : Controller
     {
-        private readonly ILogger<PetController> _logger;
+        private readonly IPetService _petService;
+
+        public PetController(IPetService petService)
+        {
+            _petService = petService;
+        }
 
         public IActionResult Index()
         {
@@ -16,7 +21,12 @@ namespace PetPal.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CreatePetViewModel model)
         {
-            return Ok();
+            var response = await _petService.Create(model);
+            if (response.StatusCode == Domain.Enum.StatusCode.OK)
+            {
+                return Ok(new { Description = response.Descriptions });
+            }
+            return BadRequest(new {description = response.Descriptions });
         }
     }
 }
